5c909741f419346b50ff391613cb027a0e74819f
Russ Ward <maddogward@gmail.com>
2018-01-22 14:30:03 -0800

    add logic to sort and return string from the server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(express.static(path.join(__dirname, '../client')));
 
+app.post('/sort', (req, res) => {
+  let charArray = req.body.string.split('');
+  //sort the string charchters and return the sorted string
+  charArray.sort();
+  res.status(200).send(charArray.join(''));
+})
+
 app.listen('8080',() => {
   console.log('app is listening on port 8080')
 });
